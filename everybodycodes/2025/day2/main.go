96c2b310402cee5a7fbb22df5144b5180499b5f9
Daan Meijers <me@daanmeijers.nl>
2025-11-07 08:37:51 +0100

    day2 extended, not working

--- everybodycodes/2025/day2/main.go
+++ everybodycodes/2025/day2/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	lib.Quest("day2", FirstPage, lib.PlaceholderFunc, lib.PlaceholderFunc)
+	lib.Quest("day2", FirstPage, EngraveBreastPlate, lib.PlaceholderFunc)
 }
 
 type ComplexNumber struct {
@@ -52,13 +52,17 @@ func ParseComplexNumber(str string) ComplexNumber {
 	}
 }
 
+func ReadInput(filename string) ComplexNumber {
+	return ParseComplexNumber(lib.ReadLine(filename))
+}
+
 func FirstPage(filename string) {
 	numbers := ReadInput(filename)
-	total := DoFirstPageArithmetic(numbers)
+	total := DoSampleCalculations(numbers)
 	fmt.Printf("The first page of calculations results in %s\n", total)
 }
 
-func DoFirstPageArithmetic(a ComplexNumber) ComplexNumber {
+func DoSampleCalculations(a ComplexNumber) ComplexNumber {
 	total := ComplexNumber{X: 0, Y: 0}
 	for i := 0; i < 3; i++ {
 		total = total.Multiply(total)
@@ -68,6 +72,37 @@ func DoFirstPageArithmetic(a ComplexNumber) ComplexNumber {
 	return total
 }
 
-func ReadInput(filename string) ComplexNumber {
-	return ParseComplexNumber(lib.ReadLine(filename))
+func EngraveBreastPlate(filename string) {
+	start := ReadInput(filename)
+	distance := 1000
+	step := 10
+	maxCycles := 100
+
+	totalEngraved := 0
+	for y := 0; y <= distance; y += step {
+		for x := 0; x <= distance; x += step {
+			current := start.Add(ComplexNumber{X: x, Y: y})
+			doEngrave := true
+			for i := 0; i < maxCycles; i++ {
+				current = current.Multiply(current)
+				fmt.Println("multiplied:", current)
+				current = current.Divide(ComplexNumber{X: 100_000, Y: 100_000})
+				fmt.Println("divided:", current)
+				current = current.Add(start)
+				fmt.Println("added:", current)
+				if current.X < -1_000_000 || current.X > 1_000_000 || current.Y < -1_000_000 || current.Y > 1_000_000 {
+					doEngrave = false
+					fmt.Printf("P=%s R=%s C=%d\n", start.Add(ComplexNumber{X: x, Y: y}), current, i)
+					break
+				}
+				return
+			}
+			if doEngrave {
+				fmt.Printf("P=%s R=%s\n", start.Add(ComplexNumber{X: x, Y: y}), current)
+				totalEngraved++
+			}
+		}
+		// fmt.Println()
+	}
+	fmt.Println("Total engraved points:", totalEngraved)
 }
