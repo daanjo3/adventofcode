fecfe7d3fdf36cc99c7e194d42025b2aaf971e37
Daan Meijers <me@daanmeijers.nl>
2025-11-13 16:22:33 +0100

    add day8p3 naive working

--- everybodycodes/2025/day8/main.go
+++ everybodycodes/2025/day8/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	lib.Quest("day8", CountCenterCrosses, CountLineCrosses)
+	lib.Quest("day8", CountCenterCrosses, CountLineCrosses, CutMaxKnots)
 }
 
 type Art struct {
@@ -16,14 +16,27 @@ type Art struct {
 	lines []Line
 }
 
+func (a *Art) Contains(line Line) bool {
+	slices.Sort(line)
+	return slices.ContainsFunc(a.lines, func(l Line) bool {
+		return l[0] == line[0] && l[1] == line[1]
+	})
+}
+
 func (a *Art) Add(line Line) int {
+	knots := a.CountCrosses(line)
+	slices.Sort(line)
+	a.lines = append(a.lines, line)
+	return knots
+}
+
+func (a *Art) CountCrosses(line Line) int {
 	knots := 0
 	for _, l := range a.lines {
 		if l.Crosses(a.size, line) {
 			knots++
 		}
 	}
-	a.lines = append(a.lines, line)
 	return knots
 }
 
@@ -49,6 +62,28 @@ func (l Line) Crosses(artSize int, other Line) bool {
 		return count == 1
 	}
 	return crosses(other[0], other[1]) || crosses(other[1], other[0])
+}
+
+func CutMaxKnots(inputfile string) {
+	numbers, max := parseStringArt(inputfile)
+	art := Art{size: max, lines: []Line{}}
+	for i := 0; i < len(numbers)-1; i++ {
+		art.Add(Line{numbers[i], numbers[i+1]})
+	}
+	crossesMax := 0
+	for i := 1; i <= max; i++ {
+		for j := 1; j <= max; j++ {
+			line := Line{i, j}
+			crosses := art.CountCrosses(line)
+			if art.Contains(line) {
+				crosses++
+			}
+			if crosses > crossesMax {
+				crossesMax = crosses
+			}
+		}
+	}
+	fmt.Printf("Max amount of lines cut is %d\n", crossesMax)
 }
 
 func CountLineCrosses(inputfile string) {
